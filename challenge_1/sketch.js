ac63466c63c6a67fd9077e46d85c1d43d95fec99
yoonjik26 <yoonjik1126@gmail.com>
2015-09-27 18:03:25 -0400

    sketch

--- challenge_1/sketch.js
+++ challenge_1/sketch.js
@@ -1,18 +1,39 @@
+var xPositions = [];
+var yPositions = [];
+
 function setup() {
 	// create a place to draw
-	createCanvas(640, 360);
+	createCanvas(640, 480);
 	noStroke();
 	noLoop();
+	colorMode(RGB);
+	background(100, 185, 235);
+	fill(57, 177, 74);
+	rect(0, 280, 640, 200);
+	for (var i = 0; i < 40; i++){
+		xPositions[i] = random(0, 640);
+		yPositions[i] = random(200, 400);
+	}
 }
-
 function draw() {
-	// clear the background
-	background(50, 50, 150);
-
-	// set a fill color
-	fill(255, 255, 255);
-
-	// draw the ellipse
-	var diameter = random(100, 200);
-	ellipse(320, 180, diameter, diameter);
+	var size = 40;
+	for (var i = 0; i < 40; i++){
+		// draw stem
+		fill(38, 120, 55);
+		rect(xPositions[i] - 2.5, yPositions[i] - 2.5, 5, 100, 20);
+		ellipse(xPositions[i], yPositions[i], 15, 15);
+		// draw flower
+		// first ring
+		fill(255, 255, 255, 85);
+		ellipse(xPositions[i], yPositions[i], size, size);
+		// second ring
+		fill(255, 255, 255, 90);
+		ellipse(xPositions[i], yPositions[i], size - 10, size - 10);
+		// third ring
+		fill(255, 255, 255, 95);
+		ellipse(xPositions[i], yPositions[i], size - 20, size - 20);
+		// fourth ring
+		fill(255, 255, 255, 100);
+		ellipse(xPositions[i], yPositions[i], size - 30, size - 30);
+	}
 }

9f1d4055aa791ace9a23492d1e920048fc1a5845
yoonjik26 <yoonjik1126@gmail.com>
2015-09-27 01:41:59 -0400

    sketch

--- challenge_2/sketch.js
+++ challenge_2/sketch.js
@@ -1,29 +1,36 @@
+var ball = {x: 500, y: 100, speedX: 5, speedY: 5, radius: 25};
+var paddle = {x: 0, y: 350, width: 300, height: 30};
+
+
 function setup() {
-	// create a place to draw
 	createCanvas(640, 480);
 	noStroke();
-	// clear the background
-	background(50, 51, 51);
 }
-var ball = {x: 500, y: 100, speedX: 5, speedY: 5, radius: 25};
-var paddle = {x: 0, y: 350. width: 300, height: 30};
-
 function draw() {
-paddle.x = mouseX;
-ellipse(ball.x, ball.y, ball.radius * 2, ball.radius * 2);
-rect(paddle.x - paddle.width * 0.5, paddle.y, paddle.width, paddle.height);
-
+	// update
+	updateBall();
+	paddle.x = mouseX;
+	// draw
+	background(50, 50, 50);
+	ellipse(ball.x, ball.y, ball.radius * 2, ball.radius * 2);
+	rect(mouseX, paddle.y, paddle.width, paddle.height);
+}
+// updateBall: move the ball, then detect and respond to collisions
+function updateBall() {
 // change position
 	ball.x = ball.x + ball.speedX;
 	ball.y = ball.y + ball.speedY;
-// right
-	if (ball.x > width - ball.radius) {
+
+	// don't let the ball go to far
+
+	// right
+	if (ball.x > 640 - ball.radius) {
 		ball.speedX = -ball.speedX;
 	}
 
 	// bottom
-	if (ball.y > height - ball.radius) {
-		ball.speedY = ball.speedY;
+	if (ball.y > 480 - ball.radius) {
+		ball.speedY = -ball.speedY;
 	}
 
 	// left
@@ -36,15 +43,10 @@ rect(paddle.x - paddle.width * 0.5, paddle.y, paddle.width, paddle.height);
 		ball.speedY = -ball.speedY;
 	}
 
-	// check for a *collision* between the ball and the paddle
-	// look up the && operator
-
-	if (ball.x > (paddle.x - paddle.width * 0.5) && ball.x < (paddle.x + paddle.width * 0.5) && ball.y > paddle.y - ball.radius) {
+	if (ball.x > (paddle.x - paddle.width) && ball.x < (paddle.x + paddle.width) && ball.y > (paddle.y - ball.radius)) {
 		if (ball.speedY > 0) {
 			ball.speedY = -ball.speedY;
 		}
 	}
 }
-
-
 

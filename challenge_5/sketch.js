3ca06c84b1110d010c6c5262e02edb8b8e89415d
yoonjik26 <yoonjik1126@gmail.com>
2015-10-02 15:26:08 -0400

    sketch

--- challenge_5/sketch.js
+++ challenge_5/sketch.js
@@ -1,23 +1,25 @@
+var mouseUpX = 0;
+var mouseUpY = 0;
+var ellipseSize = 50;
 
 function setup() {
-	// create a place to draw
 	createCanvas(640, 480);
-	// frameRate(10)
-	noStroke();
-	// noLoop();
+	fill(28, 117, 255);
+	stroke(255, 255, 255);
+	strokeWeight(10);
 }
 
 function draw() {
 	// clear the background
 	background(50, 20, 21);
 
-if (mouseIsPressed) {
-	line(mouseX, mouseY, pmouseX, pmouseY);
+if (!mouseIsPressed) {
+	mouseUpX = mouseX;
+	mouseUpY = mouseY;
 } else {
 	// set a fill color
-	fill(255, 255, 255);
-	ellipse(mouseX, mouseY, 50, 50);
-	fill(28,117, 255);
-	ellipse(mouseX, mouseY, 30, 30);
+	line(mouseUpX, mouseUpY, mouseX, mouseY);
+	ellipse(mouseUpX, mouseUpY, ellipseSize, ellipseSize);
 }	
+	ellipse(mouseX, mouseY, ellipseSize, ellipseSize);
 }
